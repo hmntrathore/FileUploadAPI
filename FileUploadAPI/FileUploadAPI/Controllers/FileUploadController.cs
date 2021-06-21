298a8c6123eed6b26ba61a6e210d4779ad0df007
hmntrathore <32383275+hmntrathore@users.noreply.github.com>
2021-06-21 10:40:35 +0530

    initial commit

---
+++ FileUploadAPI/FileUploadAPI/Controllers/FileUploadController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FileUploadAPI.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class FileUploadController : ControllerBase
+    {
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(string), StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> UploadFile(IFormFile file)
+        {
+            
+            FileUploader uploader = new FileUploader();
+            await uploader.WriteFile(file);
+            return Ok();
+
+
+        }
+
+
+        [HttpPost("Multiple", Name = "Multiple")]
+        [RequestFormLimits(MultipartBodyLengthLimit = 100000, ValueCountLimit = 2)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(string), StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> UploadFile(IFormFileCollection fileCollection)
+        {
+            FileUploader uploader = new FileUploader();
+          
+            foreach (var file in fileCollection)
+            {
+                await uploader.WriteFile(file);
+            }
+            return Ok();
+
+
+        }
+    }
+}
---
+++ FileUploadAPI/FileUploadAPI/FileUploader.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FileUploadAPI
+{
+    public class FileUploader
+    {
+        public async Task<bool> WriteFile(IFormFile file)
+        {
+            if (file == null)
+            {
+                throw new ArgumentException("please upload the file using file key");
+            }
+
+            string fileName = file.FileName;
+            string path = Path.Combine(Directory.GetCurrentDirectory(), "Upload", fileName);
+            using (var stream = new FileStream(path, FileMode.Create))
+            {
+                await file.CopyToAsync(stream);
+            }
+            return true;
+        }
+           
+        
+    }
+}
